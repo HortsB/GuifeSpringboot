4248e866dd8084a586b83ceb3c6762503c24cc80
Luigui Mejía Mendoza <50814418+HortsB@users.noreply.github.com>
2022-06-01 04:44:30 -0500

    Correción al llamado del package

--- src/main/java/pe/edu/upc/Guife_Springboot/repositories/ICountryRepository.java
+++ src/main/java/pe/edu/upc/Guife_Springboot/repositories/ICountryRepository.java
@@ -1,9 +1,9 @@
-package pe.edu.upc.guife.repositories;
+package pe.edu.upc.Guife_Springboot.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import pe.edu.upc.guife.entities.Country;
+import pe.edu.upc.Guife_Springboot.entities.Country;
 
 @Repository
 public interface ICountryRepository extends JpaRepository<Country, Integer> {
