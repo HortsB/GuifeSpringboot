022d50714e39d539322f6ad68d25818920acbd63
Luigui Mejía Mendoza <50814418+HortsB@users.noreply.github.com>
2022-06-01 04:43:57 -0500

    Correción al llamado del package

--- src/main/java/pe/edu/upc/Guife_Springboot/repositories/IAppointmentRepository.java
+++ src/main/java/pe/edu/upc/Guife_Springboot/repositories/IAppointmentRepository.java
@@ -1,9 +1,9 @@
-package pe.edu.upc.guife.repositories;
+package pe.edu.upc.Guife_Springboot.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import pe.edu.upc.guife.entities.Appointment;
+import pe.edu.upc.Guife_Springboot.entities.Appointment;
 
 @Repository
 public interface IAppointmentRepository extends JpaRepository<Appointment, Integer>{
