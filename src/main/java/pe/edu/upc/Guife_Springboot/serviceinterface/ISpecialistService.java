a0a376acb929adc82c7cd804b94570af08793926
Luigui Mejía Mendoza <50814418+HortsB@users.noreply.github.com>
2022-06-01 04:38:34 -0500

    Implementación del HUE 02 - 03

--- src/main/java/pe/edu/upc/Guife_Springboot/serviceinterface/ISpecialistService.java
+++ src/main/java/pe/edu/upc/Guife_Springboot/serviceinterface/ISpecialistService.java
@@ -1,6 +1,7 @@
 package pe.edu.upc.Guife_Springboot.serviceinterface;
 
 import java.util.List;
+import java.util.Optional;
 
 import pe.edu.upc.Guife_Springboot.entities.Specialist;
 
@@ -8,4 +9,7 @@ public interface ISpecialistService {
 	public void insert(Specialist specialist);
 
 	List<Specialist> list();
+	public void delete (int idSpecialist);
+	Optional<Specialist> listId(int idSpecialist);
+	public void update(Specialist specialist);
 }
